e7dbc650020e9374ff44ddfce7d078aaffbe1c42
Alex <Alex@COMP1>
2025-03-18 20:08:04 +0300

    Добавьте файлы проекта.

---
+++ Ganets.UI/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ganets.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
