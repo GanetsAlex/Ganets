5a16b5cb18af90c560829defddb75b37b6c3c7a0
Alex Ganets <ganetsalex@gmail.com>
2025-03-19 15:26:05 +0300

    Лаба 2 до конца

--- Ganets.UI/Controllers/HomeController.cs
+++ Ganets.UI/Controllers/HomeController.cs
@@ -1,11 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Ganets.UI.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Ganets.UI.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+        private readonly List<ListDemo> _listData;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            _listData = new List<ListDemo>
+                {
+                    new ListDemo {Id=1, Name="Item 1"},
+                    new ListDemo {Id=2, Name="Item 2"},
+                    new ListDemo {Id=3, Name="Item 3"}
+                };
+        }
         public IActionResult Index()
         {
+            {
+                ViewData["Message"] = "Лабораторная работа №2";
+            }
+
+            // Передача данных в представление через SelectList
+            ViewData["Lst"] = new SelectList(_listData, "Id", "Name");
+
             return View();
         }
     }
---
+++ Ganets.UI/Models/ListDemo.cs
@@ -0,0 +1,8 @@
+﻿namespace Ganets.UI.Models
+{
+    public class ListDemo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
