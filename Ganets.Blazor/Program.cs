234ec0723bcadb88bd69221c75a1473bd6390beb
Alex Ganets <ganetsalex@gmail.com>
2025-06-03 22:01:29 +0300

    9 лаба до конца

---
+++ Ganets.Blazor/Program.cs
@@ -0,0 +1,34 @@
+using Ganets.Blazor.Components;
+using Ganets.Blazor.Services;
+using Ganets.Domain.Entities;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddHttpClient<IProductService<Gadget>, ApiProductService>(c =>
+        c.BaseAddress = new Uri("https://localhost:7002/api/gadgets"));
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
---
+++ Ganets.Blazor/Services/ApiProductService.cs
@@ -0,0 +1,55 @@
+﻿using Ganets.Domain.Entities;
+using Ganets.Domain.Entities.Models;
+
+namespace Ganets.Blazor.Services
+
+{
+    public class ApiProductService(HttpClient Http) : IProductService<Gadget>
+    {
+        List<Gadget> _gadgets;
+        int _currentPage = 1;
+        int _totalPages = 1;
+        public IEnumerable<Gadget> Products => _gadgets;
+
+        public int CurrentPage => _currentPage;
+        public int TotalPages => _totalPages;
+
+        public event Action ListChanged;
+
+        public async Task GetProducts(int pageNo, int pageSize)
+        {
+            // Url сервиса API 
+            var uri = Http.BaseAddress.AbsoluteUri;
+
+            // данные для Query запроса 
+            var queryData = new Dictionary<string, string>
+            {
+            { "pageNo", pageNo.ToString() },
+            {"pageSize", pageSize.ToString() }
+            };
+
+            var query = QueryString.Create(queryData);
+            // Отправить запрос http 
+            var result = await Http.GetAsync(uri + query.Value);
+            // В случае успешного ответа 
+            if (result.IsSuccessStatusCode)
+            {
+                // получить данные из ответа 
+                var responseData = await result.Content
+                    .ReadFromJsonAsync<ResponseData<ListModel<Gadget>>>();
+                // обновить параметры 
+                _currentPage = responseData.Data.CurrentPage;
+                _totalPages = responseData.Data.TotalPages;
+                _gadgets = responseData.Data.Items;
+                ListChanged?.Invoke();
+            }
+            // В случае ошибки 
+            else
+            {
+                _gadgets = null;
+                _currentPage = 1;
+                _totalPages = 1;
+            }
+        }
+    }
+}
